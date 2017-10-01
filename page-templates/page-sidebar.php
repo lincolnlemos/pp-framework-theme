5698021dac385dda9631d4b5afb5082cd669e890
Lincoln de Sousa Cunha Lemos <lemoslincoln@gmail.com>
2017-10-01 18:01:41 -0300

    Update Sidebar issues

--- page-templates/page-sidebar.php
+++ page-templates/page-sidebar.php
@@ -6,37 +6,44 @@
 get_header(); ?>
 
 	<main id="main-content" class="main" role="main">		
-		<div class="container">
-			
-			<div id="sidebar" class="sidebar">
-	        <div class="sidebar__inner">
-	            <?php 
-	            	if ( is_active_sidebar( 'sidebar-principal' ) ) :
 
-	            		echo '<aside id="sidebar"';
-	            			dynamic_sidebar( 'sidebar-principal' );
-	            		echo '</aside><!-- #sidebar -->';
+		<div class="container">	
 
-	            	endif;
-	            ?>
-	        </div>
-	    </div>
-	    
-	    <div id="content" class="content">
-	       <?php 
-	        	echo get_partial('_header-search');
-	        	echo get_partial('_header-archive');
-	        	if (have_posts()): while (have_posts()) : the_post();
-	        		get_partial('_loop-page');
-	        	endwhile; 
-	        		if (function_exists('wp_pagenavi')) { 
-	        			echo '<div class="clearfix"></div>';
-	        			wp_pagenavi();
-	        		};				
-	        	endif; 
-	        	wp_reset_query(); 
-	        ?>
-	    </div>			    		
-		</div>
+			<div class="row">				
+			    
+		    <div id="content" class="content col-md-8">
+		        <?php 
+		        	echo get_partial('_header-search');
+		        	echo get_partial('_header-archive');
+		        	if (have_posts()): while (have_posts()) : the_post();
+		        		get_partial('_loop-page');
+		        	endwhile; 
+		        		if (function_exists('wp_pagenavi')) { 
+		        			echo '<div class="clearfix"></div>';
+		        			wp_pagenavi();
+		        		};				
+		        	endif; 
+		        	wp_reset_query(); 
+		        ?>
+		    </div>	
+
+    		<div id="sidebar" class="sidebar col-md-4">
+            <div class="sidebar__inner">
+                <?php 
+                	if ( is_active_sidebar( 'sidebar-principal' ) ) :
+
+                		echo '<aside id="sidebar">';
+                			dynamic_sidebar( 'sidebar-principal' );
+                		echo '</aside><!-- #sidebar -->';
+
+                	endif;
+                ?>
+            </div>
+        </div>
+
+			</div> <!-- row -->
+
+		</div> <!-- container -->
+
 	</main>
 <?php get_footer(); ?>
