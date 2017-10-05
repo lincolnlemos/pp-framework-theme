2a25dd73ccf31c830556dd5f778bb7fc5948c4af
Lincoln de Sousa Cunha Lemos <lemoslincoln@gmail.com>
2017-10-05 14:36:13 -0300

    Update Opções JSON && Disqus blocks
    Now you add Disqus short name on front-page. If not filled will not
    load Disqus.

--- page-templates/partials/blocks/_comments-disqus.php
+++ page-templates/partials/blocks/_comments-disqus.php
@@ -1,16 +1,23 @@
-<div id="disqus_thread"></div>
-<script>						    
-    var disqus_config = function () {
-        this.page.url = '<?php echo get_permalink(); ?>';
-        // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = '<?php echo get_the_id(); ?>';
-        // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };						    
-    (function() {  // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');						        
-        s.src = '//TROCAR-AQUI.disqus.com/embed.js';						        
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
-</script>
-<noscript>Ative o javascript para visualizar os comentários </noscript>
+<?php
+    $pp_disqus_shortname = get_field( 'pp_disqus_shortname', get_option( 'page_on_front' ) );
+    if ($pp_disqus_shortname) :
+?>
+
+    <div id="disqus_thread"></div>
+    <script>						    
+        var disqus_config = function () {
+            this.page.url = '<?php echo get_permalink(); ?>';
+            // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = '<?php echo get_the_id(); ?>';
+            // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };						    
+        (function() {  // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');						        
+            s.src = '//<?php echo $pp_disqus_shortname ?>.disqus.com/embed.js';						        
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <noscript>Ative o javascript para visualizar os comentários </noscript>
+
+<?php endif; // if $pp_disqus_shortname ?>
