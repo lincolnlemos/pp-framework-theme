fab6285b9e4ecf10553c529b5f1d3674a9768d5b
Lincoln de Sousa Cunha Lemos <lemoslincoln@gmail.com>
2017-10-09 07:48:19 -0300

    Default Custom Colors form TINYMCE

--- functions/wp-admin.php
+++ functions/wp-admin.php
@@ -4,6 +4,10 @@
 // YOAST
 // DASHBOARD
 // WP-LOGIN
+// TINYMCE
+  // Custom elements
+  // Custom Colors
+
 
 /* ADMIN MENU */
 /* ----------------------------------------- */
@@ -98,3 +102,76 @@ add_filter( 'admin_body_class', 'id_usuario_body_class' );
 
 
 
+
+
+/* TINYMCE */
+/* ----------------------------------------- */
+  add_filter('tiny_mce_before_init', 'tiny_mce_remove_unused_formats' ); 
+
+  function tiny_mce_remove_unused_formats($init) {
+    // Add block format elements you want to show in dropdown
+    // $init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6';
+    return $init;
+  }
+  
+  function my_mce4_options($init) {
+    $default_colours = '"000000", "Black",
+                        "993300", "Burnt orange",
+                        "333300", "Dark olive",
+                        "003300", "Dark green",
+                        "003366", "Dark azure",
+                        "000080", "Navy Blue",
+                        "333399", "Indigo",
+                        "333333", "Very dark gray",
+                        "800000", "Maroon",
+                        "FF6600", "Orange",
+                        "808000", "Olive",
+                        "008000", "Green",
+                        "008080", "Teal",
+                        "0000FF", "Blue",
+                        "666699", "Grayish blue",
+                        "808080", "Gray",
+                        "FF0000", "Red",
+                        "FF9900", "Amber",
+                        "99CC00", "Yellow green",
+                        "339966", "Sea green",
+                        "33CCCC", "Turquoise",
+                        "3366FF", "Royal blue",
+                        "800080", "Purple",
+                        "999999", "Medium gray",
+                        "FF00FF", "Magenta",
+                        "FFCC00", "Gold",
+                        "FFFF00", "Yellow",
+                        "00FF00", "Lime",
+                        "00FFFF", "Aqua",
+                        "00CCFF", "Sky blue",
+                        "993366", "Red violet",
+                        "FFFFFF", "White",
+                        "FF99CC", "Pink",
+                        "FFCC99", "Peach",
+                        "FFFF99", "Light yellow",
+                        "CCFFCC", "Pale green",
+                        "CCFFFF", "Pale cyan",
+                        "99CCFF", "Light sky blue",
+                        "CC99FF", "Plum"';
+
+    $custom_colours =  '"E14D43", "Color 1 Name",
+                        "D83131", "Color 2 Name",
+                        "ED1C24", "Color 3 Name",
+                        "F99B1C", "Color 4 Name",
+                        "50B848", "Color 5 Name",
+                        "00A859", "Color 6 Name",
+                        "00AAE7", "Color 7 Name",
+                        "282828", "Color 8 Name"';
+
+    // build colour grid default+custom colors
+    $init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']';
+
+    // enable 6th row for custom colours in grid
+    $init['textcolor_rows'] = 6;
+
+    return $init;
+  }
+  add_filter('tiny_mce_before_init', 'my_mce4_options');
+
+/* ----------------------------------------- TINYMCE */    
